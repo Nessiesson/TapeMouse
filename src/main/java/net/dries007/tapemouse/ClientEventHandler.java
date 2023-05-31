2ab452ff9bd860903c6080a6db4674fb089fe444
nessie <nessie@hotmail.dk>
2023-05-31 13:43:57 +0200

    Lazy port to 1.19.2.

--- src/main/java/net/dries007/tapemouse/ClientEventHandler.java
+++ src/main/java/net/dries007/tapemouse/ClientEventHandler.java
@@ -1,23 +1,22 @@
 package net.dries007.tapemouse;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.NewChatGui;
-import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.client.gui.screen.MainMenuScreen;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.client.gui.components.ChatComponent;
+import net.minecraft.client.gui.screens.ChatScreen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.client.event.ClientChatEvent;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static net.dries007.tapemouse.TapeMouse.LOGGER;
 import static net.minecraftforge.eventbus.api.EventPriority.HIGHEST;
@@ -28,11 +27,11 @@ import static net.minecraftforge.eventbus.api.EventPriority.HIGHEST;
  */
 public class ClientEventHandler
 {
-    private static final Map<String, KeyBinding> KEYBIND_ARRAY = ObfuscationReflectionHelper.getPrivateValue(KeyBinding.class, null, "field_74516_a");
+    private static final Map<String, KeyMapping> KEYBIND_ARRAY = ObfuscationReflectionHelper.getPrivateValue(KeyMapping.class, null, "f_90809_");
     private final Minecraft mc;
 
     private int delay;
-    private KeyBinding keyBinding;
+    private KeyMapping key;
     private int i;
 
     public ClientEventHandler()
@@ -42,7 +41,7 @@ public class ClientEventHandler
         if (KEYBIND_ARRAY == null)
         {
             RuntimeException e = new NullPointerException("KEYBIND_ARRAY was null.");
-            LOGGER.fatal("Something has gone wrong fetching the keybinding list. I guess we die now.", e);
+            LOGGER.fatal("Something has gone wrong fetching the KeyMapping list. I guess we die now.", e);
             throw e;
         }
     }
@@ -51,31 +50,31 @@ public class ClientEventHandler
      * Draw info on the screen
      */
     @SubscribeEvent
-    public void textRenderEvent(RenderGameOverlayEvent.Text event)
+    public void textRenderEvent(CustomizeGuiOverlayEvent.DebugText event)
     {
-        if (keyBinding == null) return;
-        if (mc.currentScreen instanceof MainMenuScreen || mc.currentScreen instanceof ChatScreen)
+        if (key == null) return;
+        if (mc.screen instanceof TitleScreen || mc.screen instanceof ChatScreen)
         {
             event.getLeft().add("TapeMouse paused. If you want to AFK, use ALT+TAB.");
             return;
         }
-        event.getLeft().add("TapeMouse active: " + keyBinding.getLocalizedName() + " (" + keyBinding.getKeyDescription().replaceFirst("^key\\.", "") + ')');
+        event.getLeft().add("TapeMouse active: " + key.getTranslatedKeyMessage().getString() + " (" + key.getName().replaceFirst("^key\\.", "") + ')');
         event.getLeft().add("Delay: " + i + " / " + delay);
     }
 
     /**
-     * Actually trigger the keybinding.
+     * Actually trigger the KeyMapping.
      */
     @SubscribeEvent(priority = HIGHEST)
     public void tickEvent(TickEvent.ClientTickEvent event)
     {
         if (event.phase != TickEvent.Phase.START) return;
-        if (mc.currentScreen instanceof MainMenuScreen || mc.currentScreen instanceof ChatScreen) return;
-        if (keyBinding == null) return;
+        if (mc.screen instanceof TitleScreen || mc.screen instanceof ChatScreen) return;
+        if (key == null) return;
         if (i++ < delay) return;
         i = 0;
-        if (delay == 0) KeyBinding.setKeyBindState(keyBinding.getKey(), true);
-        KeyBinding.onTick(keyBinding.getKey());
+        if (delay == 0) key.setDown(true);
+        KeyMapping.click(key.getKey());
     }
 
     /**
@@ -84,60 +83,62 @@ public class ClientEventHandler
     @SubscribeEvent(priority = HIGHEST)
     public void chatEvent(ClientChatEvent event)
     {
-        if (!event.getMessage().startsWith("/tapemouse")) return;
+        LOGGER.info("blapdsf");
+        if (!event.getMessage().startsWith("!tapemouse")) return;
+        LOGGER.info("{}", event.getMessage());
         event.setCanceled(true);
         String[] args = event.getOriginalMessage().split("\\s");
-        NewChatGui gui = mc.ingameGUI.getChatGUI();
-        gui.addToSentMessages(event.getOriginalMessage());
+        ChatComponent gui = mc.gui.getChat();
+        gui.addRecentChat(event.getOriginalMessage());
         try
         {
             handleCommand(gui, args);
         }
         catch (Exception e)
         {
-            gui.printChatMessage(new StringTextComponent("An error occurred trying to run the tapemouse command:").setStyle(new Style().setColor(TextFormatting.RED)));
-            gui.printChatMessage(new StringTextComponent(e.toString()).setStyle(new Style().setColor(TextFormatting.RED)));
+            gui.addMessage(Component.literal("An error occurred trying to run the tapemouse command:").withStyle(ChatFormatting.RED));
+            gui.addMessage(Component.literal(e.toString()).withStyle(ChatFormatting.RED));
             LOGGER.error("An error occurred trying to run the tapemouse command:", e);
         }
     }
 
-    private void handleCommand(NewChatGui gui, String[] args) throws Exception
+    private void handleCommand(ChatComponent gui, String[] args) throws Exception
     {
         switch (args.length)
         {
             default:
-                gui.printChatMessage(new StringTextComponent("TapeMouse help: ").setStyle(new Style().setColor(TextFormatting.AQUA)));
-                gui.printChatMessage(new StringTextComponent("Run '/tapemouse list' to get a list of keybindings."));
-                gui.printChatMessage(new StringTextComponent("Run '/tapemouse off' to stop TapeMouse."));
-                gui.printChatMessage(new StringTextComponent("Run '/tapemouse <binding> <delay>' to start TapeMouse."));
-                gui.printChatMessage(new StringTextComponent("  delay is the number of ticks between every keypress. Set to 0 to hold down the key."));
+                gui.addMessage(Component.literal("TapeMouse help: ").withStyle(ChatFormatting.AQUA));
+                gui.addMessage(Component.literal("Run '!tapemouse list' to get a list of keybindings."));
+                gui.addMessage(Component.literal("Run '!tapemouse off' to stop TapeMouse."));
+                gui.addMessage(Component.literal("Run '!tapemouse <binding> <delay>' to start TapeMouse."));
+                gui.addMessage(Component.literal("  delay is the number of ticks between every keypress. Set to 0 to hold down the key."));
                 return;
             case 2:
                 if (args[1].equalsIgnoreCase("off"))
                 {
-                    this.keyBinding = null;
+                    this.key = null;
                     return;
                 }
                 else if (args[1].equalsIgnoreCase("list"))
                 {
                     List<String> keys = KEYBIND_ARRAY.keySet().stream().map(k -> k.replaceFirst("^key\\.", "")).sorted().collect(Collectors.toList());
-                    gui.printChatMessage(new StringTextComponent(String.join(", ", keys)));
+                    gui.addMessage(Component.literal(String.join(", ", keys)));
                 }
                 else
                 {
-                    gui.printChatMessage(new StringTextComponent("Missing delay parameter.").setStyle(new Style().setColor(TextFormatting.RED)));
+                    gui.addMessage(Component.literal("Missing delay parameter.").withStyle(ChatFormatting.RED));
                 }
                 break;
             case 3:
             {
-                KeyBinding keyBinding = KEYBIND_ARRAY.get("key." + args[1]);
-                if (keyBinding == null)
+                KeyMapping KeyMapping = KEYBIND_ARRAY.get("key." + args[1]);
+                if (KeyMapping == null)
                 {
-                    keyBinding = KEYBIND_ARRAY.get(args[1]);
+                    KeyMapping = KEYBIND_ARRAY.get(args[1]);
                 }
-                if (keyBinding == null)
+                if (KeyMapping == null)
                 {
-                    gui.printChatMessage(new StringTextComponent(args[1] + " is not a valid keybinding.").setStyle(new Style().setColor(TextFormatting.RED)));
+                    gui.addMessage(Component.literal(args[1] + " is not a valid KeyMapping.").withStyle(ChatFormatting.RED));
                     return;
                 }
                 int delay;
@@ -148,13 +149,13 @@ public class ClientEventHandler
                 }
                 catch (Exception e)
                 {
-                    gui.printChatMessage(new StringTextComponent(args[1] + " is not a positive number or 0.").setStyle(new Style().setColor(TextFormatting.RED)));
+                    gui.addMessage(Component.literal(args[1] + " is not a positive number or 0.").withStyle(ChatFormatting.RED));
                     return;
                 }
 
                 this.delay = delay;
                 this.i = 0;
-                this.keyBinding = keyBinding;
+                this.key = KeyMapping;
             }
             break;
         }
--- src/main/java/net/dries007/tapemouse/TapeMouse.java
+++ src/main/java/net/dries007/tapemouse/TapeMouse.java
@@ -1,15 +1,13 @@
 package net.dries007.tapemouse;
 
 
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Main mod file
@@ -24,7 +22,7 @@ public class TapeMouse
     {
         // Why oh why. Why can this not be a thing in the toml or @Mod like it used to be...
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "anything. i don't care", (a, b) -> true));
         // Register client event handler only if we're on the client.
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientEventHandler::new);
     }
